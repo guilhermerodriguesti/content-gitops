b4fe8dd6e5b3807775a51712845056fb06c1338c
Guilherme Rodrigues <811215-guilhermerodrigues@users.noreply.gitlab.com>
2020-12-15 10:43:18 -0300

    Add run

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+
+echo "Deploy Flux Into Your Cluster"
 kubectl get nodes
 fluxctl version
 kubectl get pods --all-namespace
@@ -13,11 +15,17 @@ fluxctl install \
 --git-path=namespaces,workloads \
 --namespace=flux | kubectl apply -f -
 
+echo "Verify The Deployment and Obtain the RSA Key"
+
 kubectl get pods --all-namespaces
 kubectl -n flux rollout status deployment/flux
-fluxctl identity --k8s-fwd-ns flux
 
 echo "Copy that RSA key, and let's head back over to GitHub."
+#Implement the RSA Key in GitHub
+#In the GitHub user interface, make sure we're in our new repository and click on the Settings tab. In there, click Deploy keys, then click the Add deploy key button. We can give it a Title of something like GitOps Deploy Key, then paste the key we copied earlier down in the Key field. Check the Allow write access box, and then click Add key
+
+fluxctl identity --k8s-fwd-ns flux
+
 
 fluxctl sync --k8s-fwd-ns flux
 
